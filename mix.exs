e7bf3721ee71ce15a16f307fbce4b53679b40285
DEVSTOPFIX <devstopfix@gmail.com>
2022-11-28 18:58:25 +0000

    feat: error tuple short-circuit test

--- mix.exs
+++ mix.exs
@@ -7,9 +7,10 @@ defmodule Postfix.MixProject do
       deps: deps(),
       description: "Postfix evaluator",
       elixir: "~> 1.11",
+      elixirc_paths: elixirc_paths(Mix.env()),
       package: package(),
       start_permanent: Mix.env() == :prod,
-      version: "1.1.334"
+      version: "1.1.336"
     ]
   end
 
@@ -19,17 +20,20 @@ defmodule Postfix.MixProject do
     ]
   end
 
-  defp deps do
+  def deps do
     [
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
-  defp package do
+  def package do
     [
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/devstopfix/postfix-elixir"}
     ]
   end
+
+  def elixirc_paths(:test), do: ["test/helpers"] ++ elixirc_paths(:prod)
+  def elixirc_paths(_), do: ["lib"]
 end
---
+++ test/helpers/assertions.ex
@@ -0,0 +1,21 @@
+defmodule Postfix.Assertions do
+  @moduledoc "Assertion helpers (requires module with an eval/1 function)"
+
+  defmacro __using__(opts) do
+    eval_module = Keyword.fetch!(opts, :eval)
+
+    quote do
+      def eval(terms) do
+        apply(unquote(eval_module), :eval, [terms])
+      end
+
+      def assert_eval(expected, terms) do
+        assert {:ok, expected} == apply(unquote(eval_module), :eval, [terms])
+      end
+
+      def refute_eval(expected, terms) do
+        assert {:error, expected} == apply(unquote(eval_module), :eval, [terms])
+      end
+    end
+  end
+end
